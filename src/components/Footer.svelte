05bbec91603f5067d63208118ecc713a7e04d64c
Lewis <davanzo.lewis@gmail.com>
2021-07-29 16:49:10 -0400

    add footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,25 @@
 <script>
-  
+  import { onMount } from 'svelte';
+
+  let year = new Date().getFullYear();
 </script>
 
+<footer>
+  <p>
+    Copyright @kindoflew {year}
+  </p>
+</footer>
+
+<style>
+  footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 2.5rem;
+  }
+
+  p {
+    margin: 0;
+  }
+</style>
